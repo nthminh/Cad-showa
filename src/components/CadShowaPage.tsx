fbf6a43193eb73f0d4f1030a1fcbef622e60cc22
nthminh2804 <nthminh2804@gmail.com>
2026-03-01 09:57:38 +0000

    feat: Add CadShowaPage and modify .env

---
+++ src/components/CadShowaPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CadShowaPage: React.FC = () => {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <iframe src="https://main--cad-showa.us-central1.staging.vertex-ai.cloud/" className="w-full h-full"/>
+    </div>
+  );
+};
